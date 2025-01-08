37549440cf117f5ab9049d58fe74282d965cd1fc
rockop11 <rockoponce1422@gmail.com>
2025-01-08 10:15:57 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Container, Typography } from "@mui/material"
+
+
+export const App = () => {
+
+  return (
+    <Container>
+      <Typography variant='h3' textAlign={'center'}>
+        Hello World
+      </Typography>
+
+    </Container>
+  )
+}
